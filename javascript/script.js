d1603ab30b45e91afa70cd4172fd4a716dd75ae7
mukulchn <mukul_chn@hotmail.com>
2021-09-29 12:01:31 +0100

    new

--- javascript/script.js
+++ javascript/script.js
@@ -11,3 +11,14 @@ console.log(`${totalLeftToPay}`);
 
 //`The total bill is £4000 the remaining amount of money to be paid is £1652`
 console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is ${totalLeftToPay}`);
+
+//creating for loop
+for(let i=0; i <10; i++){
+    console.log(`${i}`)
+}
+
+let i=0;
+while( i <10){
+    console.log(`i = ${i}`)
+    i++;
+}
